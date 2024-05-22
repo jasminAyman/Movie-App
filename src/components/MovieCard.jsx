777408a22053f112bc478eff160c7a594fa4effe
jasminAyman <jasmineissa978@gmail.com>
2024-05-22 20:39:46 +0300

    Modify the title of the movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 function MovieCard({ movie }) {
   return (
     <div className="col-md-3 col-sm-6 mb-2">
-      <div className="card p-3 text-center">
+      <div className="card p-3 text-center h-100">
         <img
           src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
           alt=""
         />
-        <h3 className="mt-2 fs-5">{movie.title}</h3>
+        <h3 className="mt-2 fs-5">{movie.title.substring(0,5)}</h3>
         <h4 className="fs-5"> Release Date : {movie.release_date} </h4>
         <Link to={`/details/${movie.id}`} className="btn btn-warning"> Details </Link>
       </div>
